bfd51fc480bc45dc2399591aee12f76a8b1156d5
Jean Jacques Augustin <rjja@outlook.fr>
2023-08-09 21:38:24 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,52 @@
-import './App.css'
+import React, { useRef, useEffect } from 'react';
 import ProgressLG from "./components/ProgressMd.jsx";
-import ProgressSM from "./components/ProgressSM.jsx";
-import ProgressMd from "./components/ProgressMd.jsx";
-
-const stops = [{position: 0, name: 'Paris'}, {position: 20, name: 'Marseille'}, {
-    position: 40, name: 'Lyon'
-}, {position: 60, name: 'Toulouse'}, {position: 80, name: 'Nice'}, {position: 86, name: 'Nantes'}, {
-    position: 100, name: 'Strasbourg'
-},];
 
 const App = () => {
+    const stops = [
+        { position: 0, name: 'Paris' },
+        { position: 20, name: 'Marseille' },
+        { position: 40, name: 'Lyon' },
+        { position: 60, name: 'Toulouse' },
+        { position: 80, name: 'Nice' },
+        { position: 86, name: 'Nantes' },
+        { position: 100, name: 'Strasbourg' },
+    ];
     const currentPosition = 50;
 
-    return (<div>
-        <h3>Position en temps réelle du train</h3>
-        <div
-            // gerer l'overflow et fixer la largeur du container pour que le scroll soit visible
-            style={{
-                width: '800px',
-                overflowX: 'scroll',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '150px',
-                // masquer les scrollbars
-                scrollbarWidth: 'none',
-            }}
+    const scrollContainerRef = useRef();
 
-        >
+    useEffect(() => {
+        if (scrollContainerRef.current) {
+            const scrollContainer = scrollContainerRef.current;
+            const progressContainer = scrollContainer.querySelector('.stepper');
+
+            const scrollWidth = progressContainer.offsetWidth - scrollContainer.offsetWidth;
+            const scrollOffset = (currentPosition / 100) * scrollWidth;
+
+            scrollContainer.scrollLeft = scrollOffset;
+        }
+    }, [currentPosition]);
+
+    return (
+        <div>
+            <h3>Position en temps réel du train</h3>
             <div
                 style={{
-                    width: '900px'
+                    width: '800px',
+                    overflowX: 'scroll',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '150px',
+                    scrollbarWidth: 'none',
+                    msOverflowStyle: 'none',
                 }}
+                ref={scrollContainerRef} // Utilisez la référence pour le conteneur de défilement
             >
-                <ProgressMd stops={stops} currentPosition={currentPosition}/>
+                <ProgressLG stops={stops} currentPosition={currentPosition} />
             </div>
         </div>
-
-    </div>);
+    );
 };
-export default App
+
+export default App;
--- src/components/ProgressMd.jsx
+++ src/components/ProgressMd.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './ProgressLG.css';
 
-const ProgressLG = ({stops, currentPosition}) => {
+const ProgressLG = ({ stops, currentPosition }) => {
     const totalStops = stops.length;
     const stepWidth = 100 / (totalStops - 1);
 
@@ -17,25 +17,41 @@ const ProgressLG = ({stops, currentPosition}) => {
     const currentStopIndex = getCurrentStopIndex();
     const currentStop = stops[currentStopIndex];
 
-    return (<div className="progress-lg">
+    // Créez une référence pour le conteneur de défilement
+    const scrollContainerRef = useRef();
+
+    useEffect(() => {
+        if (scrollContainerRef.current) {
+            const scrollContainer = scrollContainerRef.current;
+            const progressContainer = scrollContainer.querySelector('.stepper');
+
+            const scrollWidth = progressContainer.offsetWidth - scrollContainer.offsetWidth;
+            const scrollOffset = (currentPosition / 100) * scrollWidth;
+
+            scrollContainer.scrollLeft = scrollOffset;
+        }
+    }, [currentPosition]);
+
+    return (
+        <div className="progress-lg" ref={scrollContainerRef}>
             <div className="stepper">
-                {stops.map((stop, index) => (<div
+                {stops.map((stop, index) => (
+                    <div
                         key={index}
                         className={`step ${currentStopIndex === index ? 'active' : ''}`}
-                        style={{left: `${stop.position}%`}}
+                        style={{ left: `${stop.position}%` }}
                     >
-                        {stop.position}%<br/>
+                        {stop.position}%<br />
                         {stop.name}
-                    </div>))}
-                <div
-                    className="train-marker"
-                    style={{left: `${currentPosition}%`}}
-                ></div>
+                    </div>
+                ))}
+                <div className="train-marker" style={{ left: `${currentPosition}%` }}></div>
             </div>
             <div className="progress-bar">
-                <div className="train-position" style={{width: `${currentPosition}%`}}></div>
+                <div className="train-position" style={{ width: `${currentPosition}%` }}></div>
             </div>
-        </div>);
+        </div>
+    );
 };
 
 export default ProgressLG;
